1ef0069b6aa1474ed89806a1927e72aa33772145
hellhound <hellhound@209-4>
2024-11-22 17:44:57 +0200

    Добавьте файлы проекта.

---
+++ laba6.1/Program.cs
@@ -0,0 +1,22 @@
+﻿int a, b;
+Console.Write("Введите числитель дроби: ");
+a = int.Parse(Console.ReadLine());
+Console.Write("Введите знаменатель дроби: ");
+b = int.Parse(Console.ReadLine());
+
+int Ks = NIK(a, b);
+int s = a / Ks;
+int l = b / Ks;
+
+Console.WriteLine($"Сокращенная дробь: {s}/{l}");
+
+static int NIK(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
